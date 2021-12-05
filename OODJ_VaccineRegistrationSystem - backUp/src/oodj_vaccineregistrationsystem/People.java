3816e9f0f72639b94c7f534768a2cf8e90dc4a1e
kwen38 <kwen1121@hotmail.com>
2021-12-05 22:00:08 +0800

    OODJ

---
+++ OODJ_VaccineRegistrationSystem - backUp/src/oodj_vaccineregistrationsystem/People.java
@@ -0,0 +1,70 @@
+
+package oodj_vaccineregistrationsystem;
+
+public class People {
+    private String name,phoneno,ephoneno,address,ic,password,country;
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    void setName (String name){
+        this.name = name;
+    }
+    
+    public String getPhoneNo(){
+        return this.phoneno;
+    }
+    
+    void setPhoneNo (String phoneno){
+        this.phoneno = phoneno;
+    }
+    
+    public String getEPhoneNo(){
+        return this.ephoneno;
+    }
+    
+    void setEPhoneNo(String ephoneno){
+        this.ephoneno = ephoneno;
+    }
+    
+    public String getAddress(){
+        return this.address;
+    }
+    
+    void setAdress (String address){
+        this.address = address;
+    }
+    
+    
+    public String getIC(){
+        return this.ic;
+    }
+    
+    void setIC (String ic){
+        this.ic = ic;
+    }
+    
+    public String getPassword(){
+        return this.password;
+    }
+    
+    void setPassword (String password){
+        this.password = password;
+    }
+    
+    public String getCountry(){
+        return this.country;
+    }
+    
+    void setCountry(String country){
+        this.country = country;
+    }
+    
+//    // pass name 
+//    public void getName(){
+//        
+//    }
+    
+    
+}
